b18b9af8a06839e7178170659ce318e93decb495
Luke McDuff <ljm7b2@mail.umkc.edu>
2016-09-01 23:51:47 -0500

    Include dependencies

--- Source/TwitterCollection/src/com/mnpw3d/TwitterCollect.java
+++ Source/TwitterCollection/src/com/mnpw3d/TwitterCollect.java
@@ -4,10 +4,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 
 import twitter4j.FilterQuery;
-import twitter4j.HashtagEntity;
 import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
@@ -15,9 +13,7 @@ import twitter4j.StatusListener;
 import twitter4j.TwitterObjectFactory;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
-import twitter4j.User;
 import twitter4j.conf.ConfigurationBuilder;
-import twitter4j.json.DataObjectFactory;
 
 public class TwitterCollect {
 	static BufferedWriter bw;
@@ -31,7 +27,7 @@ public class TwitterCollect {
 		cb.setOAuthConsumerSecret("esGkEi4KG9w5TY9J0s1oDKhSAGXFOn4I4iI9tVPBzAdD7K3HRZ");
 		cb.setOAuthAccessToken("796595630-EHolo0o8su2CIATVvo63MIrtHLi7njoNo5XDgSHp");
 		cb.setOAuthAccessTokenSecret("urBbRVEa55IrcA3QWxNsArvFqCiVuHtE8UknE7qmK3bWU");
-		File txtTwitter = new File("TwitterHealth.txt");
+		File txtTwitter = new File("TwitterZikka.txt");
 		if (!txtTwitter.exists()) {
 			try {
 				txtTwitter.createNewFile();
@@ -78,10 +74,20 @@ public class TwitterCollect {
 				// TODO Auto-generated method stub
 
 				String jsonTweet = TwitterObjectFactory.getRawJSON(status);
+				
 				try {
 					System.out.println(count++ + "\n");
-					bw.append(jsonTweet + "\n");
-					// bw.flush();
+					bw.append(jsonTweet);
+					bw.newLine();
+					bw.newLine();
+					bw.newLine();
+					if(count == 10)
+					{
+						bw.close();
+						fw.close();
+						System.out.println("Collection of " + count + " tweets complete.");
+						System.exit(0);
+					}
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -96,7 +102,9 @@ public class TwitterCollect {
 		};
 		FilterQuery fq = new FilterQuery();
 
-		String keywords[] = { "Disease", "diseases", "virus", "bacteria","infection","health", "blood", "injury", "cancer", "tumour", "cold", "cough", "genes", "disorder", "heart", "heartattack", "illness", "mental illness", "healthy", "unhealthy", "bone marrow dementia","bone marrow", "metabolism" , "Alzheimer's", "vaccines", "Alzheimer", "skin", "attacks", "brain", "lungs","yoga", "healing", "medicine", "cure", "zikavirus", "zika", "HIV", "AIDS", "amoeba", "prostate infection", "sinus", "throat","doctors20","mhealth","emr","ehr","HCIT","healthtech","digitalhealth","hcsm","hcr","HealthReform","Healthcosts","occupyhealthcare","healthcareforall","meaningfuluse","ACO","ICD10","hcsmin","ahima","HIMSS","physician","MedEd","mdehr","medicalbilling","patientengagement","nurses","Patient","PatientExperience","HealthTalk"};
+		String keywords[] = { "Zika", "zika", "zika virus", "Zika virus", "zika Virus", "Zika fever", "zika fever"};
+		//String keywords[] = { "cat"};
+		
 
 		fq.track(keywords);
 
